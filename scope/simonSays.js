d52cec38a95c412e71d78d7f8e9e6bf2242e696a
David Clark <david.clark.1043@gmail.com>
2022-01-12 11:33:43 -0600

    Fixed scope issue in simonSays.js by moving if statement

--- scope/simonSays.js
+++ scope/simonSays.js
@@ -1,15 +1,31 @@
 
 // Initial Incorrect Code to Fix
 
+// const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+
+// for (const location of locations) {
+
+//     if (location[0] > 2) {
+//         const invalidLocation = true
+//     }
+
+//     if (invalidLocation) {
+//         console.log("This location is invalid")
+//     }
+// }
+
+//fixed code
+// moved if(invalidLocation) to within the other if(loca..) block so that invalidLocation can be called
 const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
 
 for (const location of locations) {
 
     if (location[0] > 2) {
         const invalidLocation = true
+        if (invalidLocation) {
+            console.log("This location is invalid")
+        }
     }
 
-    if (invalidLocation) {
-        console.log("This location is invalid")
-    }
+
 }
